5ad70b136d2bedf6166d238b10cf07c49b7dea5f
konguksu <rndbsk9@swu.ac.kr>
2025-03-29 19:01:58 +0900

    init

---
+++ src/main/java/gdg/swu/planmeal/PlanmealApplication.java
@@ -0,0 +1,13 @@
+package gdg.swu.planmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanmealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanmealApplication.class, args);
+	}
+
+}
